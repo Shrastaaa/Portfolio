69001e0d6e4e60c06c82654fcce6bc0f0390a274
shrastaniraula <shrastatsarhs58@gmail.com>
2023-06-06 11:32:17 +0545

    pushorigin

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div id="" className="App">
      <p>Hello World</p>
     </div>
   );
 }
 
+// function NewApp(){
+//   return()
+// }
 export default App;
